902a9b17db1781ebeda4f6edd91e5fa1cf812e46
Junde Yhi <lmy441900@aosc.xyz>
2017-03-13 22:51:58 +0800

    Workshop I/Exercise 5: update, WIP

--- Workshop I/Exercise 5/js/seat.js
+++ Workshop I/Exercise 5/js/seat.js
@@ -1,6 +1,6 @@
-window.onload = initSeat(6,12);
+window.onload = InitSeat(12,6);
 
-function initSeat(row, col) {
+function InitSeat(row, col) {
   var alphabet = [
     "A", "B", "C", "D", "E", "F", "G",
     "H", "I", "J", "K", "L", "M", "N",
@@ -14,45 +14,53 @@ function initSeat(row, col) {
     return null;
   }
 
-  mRow = row / 2;
-  mCol = col / 2;
+  // Middle values to determine the position of Aisle and Emergency Way.
+  mRow = Math.round(row / 2);
+  mCol = Math.round(col / 2);
+
+  // Aisle width.
+  // For larger cabin width, we give an aisle width of 2, otherwise 1.
+  var widthAisle = (col > 5) ? 2 : 1;
 
   var buffer = new Array();
   buffer += "<table class=\"canvas\">" +
             "<tr>" +
             "<td>Window</td>";
-  for (let i = 0; i < col; i++) {
+
+  // Header
+  for (let i = 0, iAlpha = 0; i < col + 1; i++) {
     if (i != mCol) {
-      buffer += "<td>" + alphabet[i] + "</td>";
+      buffer += "<td>" + alphabet[iAlpha++] + "</td>"; // I KNOW EXACTLY WHAT I AM DOING.
     } else {
-      buffer += "<td colspan=\"2\">Aisle</td>";
-      --i;
+      buffer += "<td colspan=\"" + widthAisle + "\">Aisle</td>";
     }
   }
-            // "<td>A</td>" +
-            // "<td>B</td>" +
-            // "<td>C</td>" +
-            // "<td colspan=\"2\">Aisle</td>" +
-            // "<td>J</td>" +
-            // "<td>K</td>" +
-            // "<td>L</td>" +
-            // "<td>Window</td>" +
-            // "</tr>"
+  buffer += "</tr>"
 
-  // for (let i = 0; i < row; i++) {
-  //   if (i != mRow) {
-  //     for (let j = 0; j < col; j++) {
-  //       if (j != mCol) {
-  //         // Normal Seats
-  //
-  //       } else {
-  //         // Aisle
-  //       }
-  //     }
-  //   } else {
-  //     // Emergency
-  //   }
-  // }
+  // Seats
+  for (let j = 0; j < row; j++) {
+    buffer += "<tr>" + "<td>" + (j + 1) + "</td>"
+    for (let i = 0; i < col + 1; i++) {
+      if (i != mCol) {
+        if (j == mRow) {
+          // Emergency seats are not orderable.
+          buffer += "<td><button class=\"seats\"><img src=\"./img/occups.png\" /></button></td>";
+        } else {
+          buffer += "<td><button class=\"seats\"><img src=\"./img/avails.png\" /></button></td>";
+        }
+      } else {
+        for (let width = 0; width < widthAisle; width++) {
+          if (j == mRow) {
+            // Emergency marks on Aisle
+            buffer += "<td><img src=\"./img/emerg.png\" /></td>";
+          } else {
+            buffer += "<td><img src=\"./img/aisle.png\" /></td>";
+          }
+        }
+      }
+    }
+    buffer += "</tr>";
+  }
 
   buffer += "</table>"
 
